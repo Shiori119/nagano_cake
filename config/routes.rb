288db62bdd2e725b21ce7bbe7548ff3898d2f160
EC2 Default User <ec2-user@ip-172-31-89-164.ec2.internal>
2022-07-09 14:15:02 +0000

    [Add]コントローラーの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,41 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :sessions, only: [:new, :create, :destroy]
+    resources :items
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :orders_details, only: [:update]
+    get '' => 'homes#top', as: 'admin'
+  end
+
+  scope module: :public do
+    root to: 'homes#top'
+    get '/about' => 'homes#about', as: 'about'
+    resources :items, only: [:index, :show,]
+    resources :registrations, only: [:new, :create]
+    resources :sessions, only: [:new, :create, :destroy]
+
+    resources :customers, only: [:show, :edit, :update] do
+      collection do
+        get 'hide'
+        patch 'out'
+      end
+    end
+
+    resources :cart_items, only: [:index, :update, :destroy, :create] do
+      collection do
+        delete 'destroy_all'
+      end
+    end
+
+    resources :orders, only: [:new, :show, :create, :index] do
+      collection do
+        get 'comp'
+      end
+    end
+
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+
+  end
 end
